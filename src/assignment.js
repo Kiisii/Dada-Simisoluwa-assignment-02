5a03fe4631bce218d82ff400b10d1f6f115a2ea6
SimitheHotBread <47578477+Kiisii@users.noreply.github.com>
2022-07-12 14:14:20 +0000

    attempt - 1

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,13 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let sum = 0
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    sum += arrayOfNumbers[i]
+  }
+  return sum
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,15 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  let count = 0
+  for (number in arrayOfNumbers) {
+    if (arrayOfNumbers[number] % 2 == 0) {
+      count += 1
+    }
+  }
+    return count
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
@@ -47,7 +57,12 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+  // ATTEMPTED X
+  // let fahr = []
+  // for (number in arrayOfNumbers) {
+  //   fahr.push((Math.trunc(number * 1.8) + 32))
+  // }
+  // console.log(fahr)
 }
 // assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
